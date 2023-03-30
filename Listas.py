4a4658aa8a10661a87cee28c6b22f4c594a16735
unknown <roger.penhasenai@gmail.com>
2023-03-30 16:48:43 -0300

    finalizado exercício 4

--- Listas.py
+++ Listas.py
@@ -9,5 +9,12 @@ Lista2 = [1,2,3,4,5]
 Lista2.reverse()
 print(Lista2)
 
+#exercício 3
 
+Notas = {8,5,4,10,7}
 
+def CalcularNotas (Notas):
+    QuantidadeDeNotas = len(Notas)
+    Média = sum(Notas) / QuantidadeDeNotas
+    return Média
+print(CalcularNotas(Notas))
