816af0a6fb3ffc2844e84fbc15eaf0f6537a6c76
unknown <roger.penhasenai@gmail.com>
2023-04-06 15:27:35 -0300

    atividade 6 finalizada

--- Listas.py
+++ Listas.py
@@ -21,21 +21,34 @@ print(CalcularNotas(Notas))
 
 #exercício 4
 
-Lista4 = ['a, b, c, d, e, f, g, h, i, j']
-
-vogal = "a,e,i,o,u"
-
-vogal=False
-
-if vogal =! True
-
-Lista4.pop()
-
-else vogal = True
-
-print()
-
+Lista4 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+vogais = ['a','e','i','o','u']
+ListadeConsoantes = []
+consoantes = 0
+contador = 0
+for i in Lista4:
+        if not  Lista4 [contador] in vogais:
+            consoantes+=1
+            ListadeConsoantes.append(Lista4[contador])
+        contador += 1
+print(ListadeConsoantes)
+print (consoantes)
 
 
+#exercício 5
 
+Lista5 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+Pares = []
+Impares = []
+contador = 0
+
+for i in Lista5:
+    if Lista5 [contador]%2:
+        Impares.append(Lista5[contador])
+    else:
+        Pares.append(Lista5[contador])
+    contador+= 1
+print("números totais",Lista5)
+print ("números pares:",Pares)
+print ("números impares:",Impares)
 
