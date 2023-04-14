93350a02073522b5dc334fc7b1a923a2d4a3a9eb
unknown <roger.penhasenai@gmail.com>
2023-04-14 11:05:10 -0300

    Exercício 3 de repetições finalizado

--- Exercício 3.py
+++ Exercício 3.py
@@ -1,7 +1,52 @@
-Notas = {8,5,4,10,7}
+CondiçãoNome = False
+CondiçãoIdade = False
+CondiçãoSalário = False
+CondiçãoSexo = False
+CondiçãoEstadoCivil = False
 
-def CalcularNotas (Notas):
-    QuantidadeDeNotas = len(Notas)
-    Média = sum(Notas) / QuantidadeDeNotas
-    return Média
-print(CalcularNotas(Notas))
+while CondiçãoNome != True:
+    print('Digite o seu nome:')
+    Nome = input()
+    if len(Nome) > 3:
+        print('Nome inserido com sucesso')
+        CondiçãoNome = True
+    else:
+        print('Nome inválido')
+
+while CondiçãoIdade != True:
+    print('Digite a sua idade:')
+    Idade = int(input())
+    if Idade >= 0 and Idade <= 150:
+        print('Idade inserida com sucesso')
+        CondiçãoIdade = True
+    else:
+        print('Idade inválida')
+
+while CondiçãoSalário != True:
+    print('Digite o seu salário:')
+    Salário = int(input())
+    if Salário >= 0:
+        print('Salário inserida com sucesso')
+        CondiçãoSalário = True
+    else:
+        print('Salário inválido')
+
+while CondiçãoSexo != True:
+    print('Digite o seu gênero: m para masculino e f para feminino:')
+    Sexo = input()
+    if Sexo == 'm' or 'f':
+        print('Genêro inserido com sucesso')
+        CondiçãoSexo = True
+    else:
+        print('Genêro inválido')
+
+while CondiçãoEstadoCivil != True:
+    print('Digite o seu estado civil: s para solteiro,c para casado,v para viúvo(a) e d para divorciado:')
+    EstadoCivil = input()
+    
+    if EstadoCivil == 's' or 'c' or 'v' or 'd':
+        print('Estado civil inserido com sucesso')
+        CondiçãoEstadoCivil = True
+    else:
+        print('Estado civil inválido')
+
