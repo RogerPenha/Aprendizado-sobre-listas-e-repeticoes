8aef5ccdb7e1aab2686158517036f01411d098f6
unknown <roger.penhasenai@gmail.com>
2023-04-14 15:26:10 -0300

    Exercício 9 da lista de repetições finalizado

--- Exercício 9.py
+++ Exercício 9.py
@@ -1,10 +1,1 @@
-lista9 = [50,100,150,200,250,300,350,400,450,500]
-
-def SomaDeTodosOsQuadrados(inteiros):
-    soma = 0
-    for i in inteiros:
-        soma += i * i
-    return soma
-    
-print('lista dos números:' + str(lista9))
-print('soma de todos os números ao quadrado:' + str(SomaDeTodosOsQuadrados(lista9)))
+for i in range(1,51,2):print(i)
